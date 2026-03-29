ab85fa63315255864f41a365eb63ed4a447ab128
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-03-29 23:11:17 +0530

    Time: 1 ms (100%), Space: 44.1 MB (79.08%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        if(s1.equals(s2))
+            return true;
+        StringBuilder sb1=new StringBuilder(s1);
+        swap(sb1, 0, 2);
+        if(s2.equals(sb1.toString()))
+            return true;
+        swap(sb1, 1, 3);
+        if(s2.equals(sb1.toString()))
+            return true;
+        sb1=new StringBuilder(s1);
+        swap(sb1, 1, 3);
+        if(s2.equals(sb1.toString()))
+            return true;
+        swap(sb1, 0, 2);
+        if(s2.equals(sb1.toString()))
+            return true;
+        return false;
+    }
+    private void swap(StringBuilder sb, int i, int j){
+        char c=sb.charAt(i);
+        sb.setCharAt(i, sb.charAt(j));
+        sb.setCharAt(j, c);
+    }
+}
