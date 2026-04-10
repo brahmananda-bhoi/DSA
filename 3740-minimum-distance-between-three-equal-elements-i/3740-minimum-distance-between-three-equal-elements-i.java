c50fe96e20c15874029a6d9d11b54042948d0f4f
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-04-10 14:29:56 +0530

    Time: 3 ms (82.64%), Space: 45.3 MB (14.47%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -1,14 +1,27 @@
 class Solution {
     public int minimumDistance(int[] nums) {
         int ans=Integer.MAX_VALUE;
+        HashMap<Integer, List<Integer>> map=new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                for(int k=j+1; k<nums.length; k++){
-                    if(nums[i] == nums[j] && nums[j] == nums[k]){
-                        int diff=Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
-                        ans=Math.min(ans, diff);
-                    }
+            if(map.containsKey(nums[i])){
+                List<Integer>b=map.get(nums[i]);
+                b.add(i);
+                map.put(nums[i],b);
+                if(b.size()>3)
+                    b.remove(0);
+                if(b.size()==3)
+                {
+                    int c=Math.abs(b.get(0)-b.get(1))+
+                    Math.abs(b.get(1)-b.get(2))+
+                    Math.abs(b.get(2)-b.get(0));
+                    if(c<ans)
+                        ans=c;
                 }
+            }
+            else{
+                List<Integer> li=new ArrayList<>();
+                li.add(i);
+                map.put(nums[i], li);
             }
         }
         return ans==Integer.MAX_VALUE?-1:ans;
