01bbbdedc0d41a470c5209b03c2da0065d6754b1
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-04-24 07:58:50 +0530

    Time: 15 ms (5.16%), Space: 79.1 MB (6.63%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if(bills[0] != 5)
+            return false;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(5, 1);
+        map.put(10, 0);
+        for(int i=1; i<bills.length; i++){
+            if(bills[i] == 5)
+                map.put(5, map.get(5)+1);
+            else if(bills[i] == 10){
+                map.put(10, map.get(10)+1);
+                if(map.get(5) == 0)
+                    return false;
+                else
+                    map.put(5, map.get(5)-1);
+            }
+            else{
+                if(map.get(5) == 0)
+                    return false;
+                if(map.get(10) != 0){
+                    map.put(10, map.get(10)-1);
+                    map.put(5, map.get(5)-1);
+                }
+                else{
+                    if(map.get(5) < 3)
+                        return false;
+                    map.put(5, map.get(5) -3);
+                }
+            }
+        }
+        return true;
+    }
+}
