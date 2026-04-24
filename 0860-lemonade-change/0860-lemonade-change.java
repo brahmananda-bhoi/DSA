e66977946c6094b530cb0dfc1c1ddd81807c49c0
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-04-24 08:00:53 +0530

    Time: 2 ms (99.74%), Space: 72.5 MB (32.16%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,34 +1,62 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        if(bills[0] != 5)
-            return false;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(5, 1);
-        map.put(10, 0);
-        for(int i=1; i<bills.length; i++){
-            if(bills[i] == 5)
-                map.put(5, map.get(5)+1);
-            else if(bills[i] == 10){
-                map.put(10, map.get(10)+1);
-                if(map.get(5) == 0)
-                    return false;
-                else
-                    map.put(5, map.get(5)-1);
+        int five=0,ten=0;
+        for(int i=0;i<bills.length;i++)
+        {
+            if(bills[i]==5)
+                five++;
+            else if(bills[i]==10){
+                ten++;
+                five--;
             }
-            else{
-                if(map.get(5) == 0)
-                    return false;
-                if(map.get(10) != 0){
-                    map.put(10, map.get(10)-1);
-                    map.put(5, map.get(5)-1);
-                }
-                else{
-                    if(map.get(5) < 3)
-                        return false;
-                    map.put(5, map.get(5) -3);
-                }
+            else if(ten>0){
+                ten--;
+                five--;
             }
+            else 
+                five-=3;
+            if(five<0)
+                return false;
         }
         return true;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public boolean lemonadeChange(int[] bills) {
+//         if(bills[0] != 5)
+//             return false;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         map.put(5, 1);
+//         map.put(10, 0);
+//         for(int i=1; i<bills.length; i++){
+//             if(bills[i] == 5)
+//                 map.put(5, map.get(5)+1);
+//             else if(bills[i] == 10){
+//                 map.put(10, map.get(10)+1);
+//                 if(map.get(5) == 0)
+//                     return false;
+//                 else
+//                     map.put(5, map.get(5)-1);
+//             }
+//             else{
+//                 if(map.get(5) == 0)
+//                     return false;
+//                 if(map.get(10) != 0){
+//                     map.put(10, map.get(10)-1);
+//                     map.put(5, map.get(5)-1);
+//                 }
+//                 else{
+//                     if(map.get(5) < 3)
+//                         return false;
+//                     map.put(5, map.get(5) -3);
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
