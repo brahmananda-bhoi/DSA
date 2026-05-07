e4594fdcd2bc043ce7c21d3d73aabb7d31ed4a36
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-05-07 19:19:55 +0530

    Time: 1 ms (99.27%), Space: 47 MB (76.95%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -14,25 +14,46 @@
  * }
  */
 class Solution {
+    private void dfs(TreeNode root, int level, List<Integer> ans){
+        if(root == null)
+            return;
+        if(level < ans.size()){
+            int max = Math.max(ans.get(level), root.val);
+            ans.set(level, max);
+        }
+        else
+            ans.add(root.val);
+        dfs(root.left, level+1, ans);
+        dfs(root.right, level+1, ans);
+    }
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if(root == null)
-            return ans;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            int max = Integer.MIN_VALUE;
-            for(int i=0; i< size; i++){
-                TreeNode cur = q.poll();
-                max = Math.max(max, cur.val);
-                if(cur.left != null)
-                    q.offer(cur.left);
-                if(cur.right != null)
-                    q.offer(cur.right);
-            }
-            ans.add(max);
-        }
+        dfs(root, 0, ans);
         return ans;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> largestValues(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root == null)
+//             return ans;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             int max = Integer.MIN_VALUE;
+//             for(int i=0; i< size; i++){
+//                 TreeNode cur = q.poll();
+//                 max = Math.max(max, cur.val);
+//                 if(cur.left != null)
+//                     q.offer(cur.left);
+//                 if(cur.right != null)
+//                     q.offer(cur.right);
+//             }
+//             ans.add(max);
+//         }
+//         return ans;
+//     }
+// }
