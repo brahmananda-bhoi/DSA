4dbaf4ac2f8afde0f36dd2c7748c4e14ab3c3ed8
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-05-13 09:41:06 +0530

    Time: 1 ms (95.54%), Space: 44.7 MB (61.87%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -1,21 +1,51 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        StringBuilder sb = new StringBuilder();
-        int l = 0, n = s.length();
-        boolean flag = true;
-        while(l < n){
-            int r = l + k - 1 < n ? l + k - 1 : n-1;
-            if(flag){
-                while(r >= l)
-                    sb.append(s.charAt(r--)+"");
-                l += k;
-            }
-            else{
-                while(l <= r)
-                    sb.append(s.charAt(l++)+"");
-            }
-            flag = !flag;
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        for (int i = 0; i < n; i += 2 * k) {
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1);
+            reverse(arr, left, right);
         }
-        return sb.toString();
+        return new String(arr);
+    }
+    private void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public String reverseStr(String s, int k) {
+//         StringBuilder sb = new StringBuilder();
+//         int l = 0, n = s.length();
+//         boolean flag = true;
+//         while(l < n){
+//             int r = l + k - 1 < n ? l + k - 1 : n-1;
+//             if(flag){
+//                 while(r >= l)
+//                     sb.append(s.charAt(r--)+"");
+//                 l += k;
+//             }
+//             else{
+//                 while(l <= r)
+//                     sb.append(s.charAt(l++)+"");
+//             }
+//             flag = !flag;
+//         }
+//         return sb.toString();
+//     }
+// }
