e6b71e107822075e98bc31d56756daa4629538fd
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-05-16 09:26:08 +0530

    Time: 4 ms (88.26%), Space: 46.7 MB (64.34%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,18 +1,33 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<Character> stk = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
-            if(c == ' '){
-                while(!stk.isEmpty())
-                    sb.append(stk.pop()+"");
-                sb.append(" ");
-            }
-            else
-                stk.push(c);
+        String sp[] = s.split(" ");
+        StringBuilder reverse = new StringBuilder();
+        for(String word : sp){
+            StringBuilder rev = new StringBuilder(word);
+            rev.reverse();
+            reverse.append(rev).append(" ");
         }
-        while(!stk.isEmpty())
-            sb.append(stk.pop()+"");
-        return sb.toString();
+        return reverse.toString().trim();
     }
 }
+
+
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         Stack<Character> stk = new Stack<>();
+//         StringBuilder sb = new StringBuilder();
+//         for(char c : s.toCharArray()){
+//             if(c == ' '){
+//                 while(!stk.isEmpty())
+//                     sb.append(stk.pop()+"");
+//                 sb.append(" ");
+//             }
+//             else
+//                 stk.push(c);
+//         }
+//         while(!stk.isEmpty())
+//             sb.append(stk.pop()+"");
+//         return sb.toString();
+//     }
+// }
