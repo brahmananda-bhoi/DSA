367a9708c677718303d4330053b1d768ff376a7f
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-07-04 10:55:50 +0530

    Time: 23 ms (78.07%), Space: 165.9 MB (34.65%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minScore(int n, int[][] roads) {
-        List<int[]>[] graph = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++)
+        List<int[]>[] graph = new ArrayList[n+1];
+        for(int i=0; i<=n; i++)
             graph[i] = new ArrayList<>();
-        for (int[] road : roads) {
+        for(int[] road : roads){
             graph[road[0]].add(new int[]{road[1], road[2]});
             graph[road[1]].add(new int[]{road[0], road[2]});
         }
-        boolean[] visited = new boolean[n + 1];
-        int minScore = Integer.MAX_VALUE;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        visited[1] = true;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
+        boolean[] vis = new boolean[n+1];
+        int score = Integer.MAX_VALUE;
+        Queue<Integer> q = new LinkedList<>();
+        vis[1] = true;
+        q.offer(1);
+        while(!q.isEmpty()){
+            int node = q.poll();
             for(int[] edge : graph[node]){
                 int next = edge[0];
                 int cost = edge[1];
-                minScore = Math.min(minScore, cost);
-                if(!visited[next]){
-                    visited[next] = true;
-                    queue.offer(next);
+                score = Math.min(score, cost);
+                if(!vis[next]){
+                    vis[next] = true;
+                    q.offer(next);
                 }
             }
         }
-        return minScore;
+        return score;
     }
 }
