0d19b387d52ef0864adca76028e734e3a7bf62ea
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-07-10 08:50:28 +0530

    Time: 1 ms (94.88%), Space: 46.1 MB (97.54%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int mid = nums[nums.length/2];
+        int count = 0;
+        for(int i : nums){
+            if(i == mid)
+                count++;
+            if(count > 1)
+                return false;
+        }
+        return true;
+    }
+}
