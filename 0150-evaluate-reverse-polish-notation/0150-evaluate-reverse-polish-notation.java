e1694054809abe09f586ca2e3f8cfedd92fc427f
Brahmananda Bhoi <brahmanandabhoi234@gmail.com>
2026-09-06 10:30:47 +0530

    Time: 6 ms (87.31%), Space: 45.2 MB (62.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,29 +1,68 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stk=new Stack<>();
-        String str="+-*/";
-        for(String s:tokens){
-            if(str.contains(s)){
-                int right=stk.pop();
-                int left=stk.pop();
-                switch(s){
-                    case "+":
-                        stk.push(left+right);
-                        break;
-                    case "-":
-                        stk.push(left-right);
-                        break;
-                    case "*":
-                        stk.push(left*right);
-                        break;
-                    case "/":
-                        stk.push(left/right);
-                        break;
-                }
+        Stack<Integer> stk = new Stack<>();
+        int a, b;
+        for(String s : tokens){
+            if(s.equals("+")){
+                a=stk.pop();
+                b=stk.pop();
+                stk.push(a+b);
+            }
+            else if(s.equals("-")){
+                a=stk.pop();
+                b=stk.pop();
+                stk.push(b-a);
+            }
+            else if(s.equals("*")){
+                a=stk.pop();
+                b=stk.pop();
+                stk.push(a*b);
+            }
+            else if(s.equals("/")){
+                a=stk.pop();
+                b=stk.pop();
+                stk.push(b/a);
             }
             else
                 stk.push(Integer.parseInt(s));
         }
+        return stk.peek();
+    }
+}
+
+
+
+
+/*
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack <Integer> stk=new Stack<>();
+        int res=0,a,b;
+        for(String c : tokens){
+            if(c.matches("-?\\d+")){
+                stk.push(Integer.parseInt(c));
+            }
+            else{
+                a=stk.pop();
+                b=stk.pop();
+                switch (c) {
+                    case "+":
+                        res =  b + a; 
+                        break;
+                    case "-": 
+                        res =  b - a; 
+                        break;
+                    case "*": 
+                        res =  b * a; 
+                        break;
+                    case "/": 
+                        res =  b / a; 
+                        break;
+                }
+                stk.push(res);
+            }
+        }
         return stk.pop();
     }
 }
+*/
